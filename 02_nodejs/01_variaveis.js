8068411eea3e9361e58eba10f0de2e567b5c7887
brenombastos <brenodead@gmail.com>
2020-06-10 15:17:37 -0300

    Primeira aula impacta

---
+++ 02_nodejs/01_variaveis.js
@@ -0,0 +1,5 @@
+let curso = 'javascript';
+let duracao = 88;
+
+console.log('Curso: ' + curso);
+console.log('Duração: ' + duracao);
---
+++ 02_nodejs/02_parametros.js
@@ -0,0 +1,5 @@
+//Neste programa, estudaremos o comportamento e o tipó esperado dos parametros das funções
+//Parâmetro de funções: informação (valor) complementar para que a função seja executada.
+function executar1(){
+
+}
