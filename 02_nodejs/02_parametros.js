aa16241d92a1a09fb74c52b3ab514c3a2fc263cb
brenombastos <brenodead@gmail.com>
2020-06-12 08:14:56 -0300

    Criado novo HTML funcionario 04

--- 02_nodejs/02_parametros.js
+++ 02_nodejs/02_parametros.js
@@ -1,5 +1,21 @@
 //Neste programa, estudaremos o comportamento e o tipó esperado dos parametros das funções
 //Parâmetro de funções: informação (valor) complementar para que a função seja executada.
-function executar1(){
-
+function executarSoma(v1, v2) {
+    let soma = v1 + v2;
+    return soma;
 }
+
+function mostrarTamnho(s) {
+    return s.length;
+}
+
+function executar(operacao) {
+    return operacao(2, 3);
+}
+
+console.log("Função executar soma: " + executarSoma(20, 35));
+console.log("Função mostrar tamanho: " + mostrarTamnho(100));
+
+
+
+//console.log(executarSoma("Breno ", "Bastos"));
